d7a61a0ebfa92b53541826d8ca173cb1e3169861
Niklas Noldin <niklas.noldin@gmail.com>
2022-09-12 11:56:53 +0200

    install vue-i18n

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,22 @@
 import axios from "axios";
+import { createI18n } from "vue-i18n";
+
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 export default function bootstrap(app) {
+    const i18nApp = createI18n({
+        locale: document.documentElement.lang,
+        fallbackLocale: "de",
+        legacy: false,
+        messages: {
+            de: {},
+            en: {},
+        },
+    });
+
+    app.use(i18nApp);
+
     app.config.globalProperties.$route = window.route;
 }
